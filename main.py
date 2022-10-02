384f08226360829e6fd416c201b9feae6acd0c72
HITESH KUMAR <73693548+thehtk@users.noreply.github.com>
2022-10-02 19:40:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ warnings.filterwarnings("ignore")
 df =pd.read_csv("boston.csv")
 X=df[['nox','rm','dis','ptratio','lstat']]
 y=df['medv']
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
-regressor.fit(X_train,y_train)
+regressor.fit(X,y)
 
 import streamlit as st
 import pickle
